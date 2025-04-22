edbcfdf4eaf4cc4e4f72184f95e7762151d17af2
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-22 18:36:19 -0300

    feat: estrutura inicial da classe TravelAgents

---
+++ models/agents.py
@@ -0,0 +1,12 @@
+class TravelAgents:
+    def __init__(self):
+        pass
+
+    def expert_travel_agent(self):
+        pass
+
+    def city_selection_expert(self):
+        pass
+
+    def local_tour_guide(self):
+        pass
