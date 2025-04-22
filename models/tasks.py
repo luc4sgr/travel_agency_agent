35a6e7841e7fe9258a6c564627d26a9731f36449
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-22 18:36:37 -0300

    feat: estrutura inicial da classe TravelTasks

---
+++ models/tasks.py
@@ -0,0 +1,12 @@
+class TravelTasks:
+    def __init__(self):
+        pass
+
+    def plan_itinerary(self, agent, city, travel_dates, interests):
+        pass
+
+    def identify_city(self, agent, origin, cities, interests, travel_dates):
+        pass
+
+    def gather_city_info(self, agent, city, travel_dates, interests):
+        pass
